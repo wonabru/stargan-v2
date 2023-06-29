efd40f5d2c316e41d6504b997e150de33e1fdb29
wonabru <wonabru@gmail.com>
2023-06-29 22:28:31 +0200

    Update eval.py

--- metrics/eval.py
+++ metrics/eval.py
@@ -67,6 +67,7 @@ def calculate_metrics(nets, args, step, mode):
 
                 # generate 10 outputs from the same input
                 group_of_images = []
+                iter_ref = iter(loader_ref)
                 for j in range(args.num_outs_per_domain):
                     if mode == 'latent':
                         z_trg = torch.randn(N, args.latent_dim).to(device)
